30fdd572db4c3373dec976baa63034393e6ece57
pjdijxhoorn <pjdijxhoorn@hotmail.com>
2021-08-10 06:58:49 +0000

    added a base template and som crude pages

---
+++ run.py
@@ -0,0 +1,32 @@
+import os
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/recipe")
+def recipe():
+    return render_template("recipe.html")
+
+
+@app.route("/search")
+def search():
+    return render_template("search.html")
+
+@app.route("/login")
+def login():
+    return render_template("login.html")    
+
+
+
+if __name__ == "__main__":
+    app.run(
+        host=os.environ.get("IP", "0.0.0.0"),
+        port=int(os.environ.get("PORT", "5000")),
+        debug=True)
