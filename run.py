6d4a506053f409c3cfe05afc904f0008a9a70909
pjdijxhoorn <pjdijxhoorn@hotmail.com>
2021-08-12 13:27:16 +0000

    added login page functionality

--- run.py
+++ run.py
@@ -33,8 +33,29 @@ def recipe():
 def search():
     return render_template("search.html")
 
-@app.route("/login")
+@app.route("/login", methods=["GET", "POST"])
 def login():
+    if request.method == "POST":
+        # check if username exists 
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+
+        if existing_user:
+            # does password match user input
+            if check_password_hash(
+                existing_user["password"], request.form.get("password")):
+                    session["user"] = request.form.get("username").lower()
+                    flash("Welcome, {}".format(request.form.get("username")))
+            else:
+                # invalid password match
+                flash("Incorrect Username and/or Password")
+                return redirect(url_for("login"))
+
+        else:
+            # username doesn't exist
+            flash("Incorrect Username and/or Password")
+            return redirect(url_for("login"))
+
     return render_template("login.html")    
 
 @app.route("/logout")
@@ -51,7 +72,8 @@ def register():
         # check if username already exists in db
         existing_user = mongo.db.users.find_one(
             {"username": request.form.get("username").lower()})
-        existing_email = mongo.db.users.email.find_one(
+        # check if email already exists in db
+        existing_email = mongo.db.users.find_one(
             {"email": request.form.get("email")})
 
         password = request.form.get("password")
